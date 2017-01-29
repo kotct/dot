0510331a38ca5e075c92757d41db041e8c262518
Christopher Cooper <christopher.c.cooper@gmail.com>
2017-01-29 09:42:56 -0600

    add scratch message

--- .emacs.d/lisp/visual/startup-c.el
+++ .emacs.d/lisp/visual/startup-c.el
@@ -2,4 +2,17 @@
 ;;; prevent display of minibuffer message
 (defun display-startup-echo-area-message ())
 
+;;; scratch message
+(defvar *inital-scratch-message-value* initial-scratch-message)
+
+;; use a hook so that we can use emacs-init-time
+(add-hook 'after-init-hook
+          (lambda ()
+            (if (equal initial-scratch-message *inital-scratch-message-value*)
+                (setq initial-scratch-message
+                      (concat ";; init: " (emacs-init-time) (format-time-string " @ %F %T")
+                              "\n;; " (user-login-name) "@" (system-name) " loaded config for " kotct/user-current-username
+                              "\n;; need help? try <C-h ?>"
+                              "\n;; scratch away\n\n")))))
+
 (provide 'startup-c)

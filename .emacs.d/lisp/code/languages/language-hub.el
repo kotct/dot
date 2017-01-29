57a0c6e78bd904d1e7fdb1fed7538691ae06c934
Christopher Cooper <christopher.c.cooper@gmail.com>
2017-01-29 13:25:03 -0600

    set up language hub

---
+++ .emacs.d/lisp/code/languages/language-hub.el
@@ -0,0 +1,14 @@
+(defvar languages
+  '(elisp
+    c)
+  "A list of packages configured by the language-hub.")
+
+(let ((to-compile ()))
+  (dolist (feature languages)
+    (setf to-compile
+          (cons (concat (file-name-directory load-file-name) (symbol-name feature) ".el") to-compile))
+    (require feature))
+  (setf kotct/files-to-compile
+        (append kotct/files-to-compile to-compile)))
+
+(provide 'language-hub)

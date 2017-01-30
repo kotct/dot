c426d257c7dd3c009ae137b42b86925961ad8036
Kristofer Rye <kristofer.rye@gmail.com>
2017-01-29 20:00:22 -0600

    Add some comments

--- .emacs.d/lisp/visual/text-visuals.el
+++ .emacs.d/lisp/visual/text-visuals.el
@@ -1,8 +1,13 @@
+;; Minor mode to provide visual feedback for
+;; some operations.
 (require 'volatile-highlights)
 (volatile-highlights-mode +1)
 
+;; Minor mode which displays search information
+;; in the mode line.
 (global-anzu-mode +1)
 
+;; Any matching parenthesis is highlighted
 (show-paren-mode +1)
 
 (provide 'text-visuals)

1757c2ce2549955c69018e4eb053bed3ef31b39d
Kristofer Rye <kristofer.rye@gmail.com>
2017-01-31 17:22:32 -0600

    Add documentation to Ruby support

--- .emacs.d/lisp/code/languages/ruby.el
+++ .emacs.d/lisp/code/languages/ruby.el
@@ -1,11 +1,21 @@
+;; Set ruby-indent-line to global-tab-width
 (setq-default ruby-indent-line global-tab-width)
 
+;; Provide advice to Smart Tabs about how to handle Ruby's special
+;; indentation variables.
 (smart-tabs-advice ruby-indent-line ruby-indent-level)
+
+;; Apply Ruby language support to Smart Tabs.
 (smart-tabs-insinuate 'ruby)
 
+;; Use tabs mode in Ruby by default.
 (setf ruby-indent-tabs-mode t)
+
+;; Don't use SMIE because SMIE sucks.
 (setf ruby-use-smie nil)
 
+;; God configurations are in Ruby.
+;; TODO Add more Ruby-esque extensions.
 (add-to-list 'auto-mode-alist '("\\.god\\'" . ruby-mode))
 
 (provide 'ruby)

163169394f2659f79f31d867a7c87cf517f632d8
Kristofer Rye <kristofer.rye@gmail.com>
2017-01-31 19:16:45 -0600

    Use smart-tabs-add-language-support
    ayy

--- .emacs.d/lisp/code/languages/ruby.el
+++ .emacs.d/lisp/code/languages/ruby.el
@@ -1,9 +1,15 @@
 ;; Set ruby-indent-level to global-tab-width
 (setq-default ruby-indent-level global-tab-width)
 
-;; Provide advice to Smart Tabs about how to handle Ruby's special
-;; indentation variables.
-(smart-tabs-advice ruby-indent-line ruby-indent-level)
+;; ;; Provide advice to Smart Tabs about how to handle Ruby's special
+;; ;; indentation variables.
+;; (smart-tabs-advice ruby-indent-line ruby-indent-level)
+
+;; Add Smart Tabs language support for Ruby using the
+;; `smart-tabs-add-language-support' macro.
+(smart-tabs-add-language-support ruby ruby-mode-hook
+  ((ruby-indent-line . ruby-indent-level)
+   (ruby-indent-level . ruby-indent-level)))
 
 ;; Apply Ruby language support to Smart Tabs.
 (smart-tabs-insinuate 'ruby)

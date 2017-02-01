470d60d265d64d92374c66c70984f4dae6ea13d2
Kristofer Rye <kristofer.rye@gmail.com>
2017-02-01 14:45:39 -0600

    Disable Smart Tabs if indent-tabs-mode is disabled by editorconfig
    This new function is run as an EditorConfig custom hook, so it only
    takes place in situations where EditorConfig can be loaded.

--- .emacs.d/lisp/code/editorconfig-c.el
+++ .emacs.d/lisp/code/editorconfig-c.el
@@ -1,4 +1,12 @@
 ;; Enable EditorConfig mode
 (editorconfig-mode +1)
 
+(defun kotct/editorconfig--disable-smart-tabs-if-indent-tabs-mode-disabled (&optional hash)
+  "Disables `smart-tabs-mode' if `indent-tabs-mode' is non-truthy."
+  (if (not indent-tabs-mode)
+      (progn
+        (smart-tabs-mode -1))))
+
+(add-hook 'editorconfig-custom-hooks 'kotct/editorconfig--disable-smart-tabs-if-indent-tabs-mode-disabled)
+
 (provide 'editorconfig-c)

be3a935ca091dd9778e13c42cfcb07f5056e914c
Kristofer Rye <kristofer.rye@gmail.com>
2017-02-03 14:02:45 -0600

    Add caveat about HASH

--- .emacs.d/lisp/code/editorconfig-c.el
+++ .emacs.d/lisp/code/editorconfig-c.el
@@ -1,8 +1,11 @@
 ;; Enable EditorConfig mode
 (editorconfig-mode +1)
 
-(defun kotct/editorconfig--disable-smart-tabs-if-indent-tabs-mode-disabled (&optional hash)
-  "Disables `smart-tabs-mode' if `indent-tabs-mode' is non-truthy."
+(defun kotct/editorconfig-disable-smart-tabs-if-indent-tabs-mode-disabled (&optional hash)
+  "Disables `smart-tabs-mode' if `indent-tabs-mode' is non-truthy.
+
+HASH is passed in when `editorconfig-custom-hooks' is evaluated,
+and is ignored."
   (if (not indent-tabs-mode)
       (progn
         (smart-tabs-mode -1))))

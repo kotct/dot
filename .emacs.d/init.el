083035582046373122adbd9fd2a7b99292583faf
Kristofer Rye <kristofer.rye@gmail.com>
2017-02-04 15:54:54 -0600

    Remove auto-generated comment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,10 +2,7 @@
 
 ;;; autoload configuration
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Initialize the package repository.
 (package-initialize)
 
 (setf generated-autoload-file "~/.emacs.d/lisp/kotct-loaddefs.el")

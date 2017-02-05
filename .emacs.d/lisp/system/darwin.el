75a2b409c4c5c3c9fdfc75615c4be2892a48e618
Kristofer Rye <kristofer.rye@gmail.com>
2017-02-05 09:08:54 -0600

    Add configuration variable and improve documentation

--- .emacs.d/lisp/system/darwin.el
+++ .emacs.d/lisp/system/darwin.el
@@ -1,16 +1,26 @@
-(defun kotct/darwin-swap-command-option ()
-  "This function swaps the Command and Option modifiers on MacOS.
+(defvar kotct/darwin-swap-command-option
+  t
+  "Determines whether `kotct/darwin-maybe-swap-command-option'
+actually swaps command and option.")
 
-This is intended to make the keyboard layout more friendly for
-those used to a PC-like keyboard layout.  This makes C- and M-
-more spread apart and makes the keyboard more ergonomic.
+;; This is intended to make the keyboard layout more friendly for
+;; those who are used to a PC-like keyboard layout.  This makes C- and
+;; M- more spread apart and makes the keyboard more ergonomic.
+;;
+;; It uses the `mac-command-modifier' and `mac-option-modifier'
+;; builtins, and so will not affect Emacs installations that were
+;; built on systems with these not enabled.
+(defun kotct/darwin-maybe-swap-command-option ()
+  "This function swaps the Command and Option modifiers on MacOS if
+`kotct/darwin-swap-command-option' is truthy.
 
-Because it uses the `mac-command-modifier' and
-`mac-option-modifier' builtins, it does not affect other systems
-built without compatibility for these two variables."
-  (progn
-    (setf mac-command-modifier 'meta)
-    (setf mac-option-modifier 'super)))
+This only affects systems with `mac-command-modifier' and
+`mac-option-modifier' built in. (i.e. NS builds, e.g. Homebrewed
+Emacs)"
+  (if kotct/darwin-swap-command-option
+      (progn
+        (setf mac-command-modifier 'meta)
+        (setf mac-option-modifier 'super))))
 
 (defun kotct/darwin-load-path-from-shell ()
   "This function sets `exec-path' to the 'PATH' variable
@@ -37,7 +47,7 @@ stuff.  This also respects the user's shell decision."
 
 (if (eq system-type 'darwin)
     (progn
-      (kotct/darwin-swap-command-option)
+      (kotct/darwin-maybe-swap-command-option)
       (kotct/darwin-load-path-from-shell)))
 
 (provide 'darwin)

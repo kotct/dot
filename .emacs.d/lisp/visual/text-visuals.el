b61198cccb45fcb7de03ce1e4e37eb112bdb625b
Kristofer Rye <kristofer.rye@gmail.com>
2017-03-05 13:50:27 -0600

    Add font rescaling code that beats the builtin Emacs
    Builtin emacs font rescaling does weird shit with linum-mode.  This doesn't.

--- .emacs.d/lisp/visual/text-visuals.el
+++ .emacs.d/lisp/visual/text-visuals.el
@@ -10,14 +10,14 @@
 ;; Minor mode which highlights TODO, FIXME, DONE, FAIL, etc.
 (add-hook 'prog-mode-hook 'hl-todo-mode)
 
+(defvar kotct/font-default-height
+  12.0
+  "The default font height.")
+
 ;; Sets the font name to NAME.
 (defun kotct/font-set-name (name)
   "Sets the `:font' face attribute on the `default' face to match NAME."
   (set-face-attribute 'default nil :font name))
-
-(defvar kotct/font-default-height
-  12.0
-  "The default font height.")
 
 ;; Sets the font height to what a user would expect their value to map to.
 ;;
@@ -29,8 +29,33 @@
   (interactive "nNew font height: ")
   (set-face-attribute 'default nil :height (floor (* 10.0 height))))
 
-;; Set the font height to the default height.
-(kotct/font-set-height kotct/font-default-height)
+(defvar kotct/font-rescale-factor
+  1.0
+  "The proportion that the font height change is multiplied by.")
+
+(defun kotct/font-rescale-height (amount)
+  "Sets the `:height' face attribute on the `default' face to
+`kotct/font-rescale-factor' * AMOUNT + the current height."
+  (let ((new-size (+ (* kotct/font-rescale-factor amount)
+                     (/ (face-attribute 'default :height) 10.0))))
+    (set-face-attribute 'default nil :height (floor (* 10.0 new-size)))))
+
+(defun kotct/font-upscale-height () (interactive "*") (kotct/font-rescale-height +1.0))
+(defun kotct/font-downscale-height () (interactive "*") (kotct/font-rescale-height -1.0))
+(defun kotct/font-reset-height () (interactive "*") (kotct/font-set-height kotct/font-default-height))
+
+(global-unset-key (kbd "C-x C-+"))
+(global-unset-key (kbd "C-x C-="))
+(global-unset-key (kbd "C-x C--"))
+(global-unset-key (kbd "C-x C-0"))
+
+(global-set-key (kbd "C-x C-+") #'kotct/font-upscale-height)
+(global-set-key (kbd "C-x C-=") #'kotct/font-upscale-height)
+(global-set-key (kbd "C-x C--") #'kotct/font-downscale-height)
+(global-set-key (kbd "C-x C-0") #'kotct/font-reset-height)
+
+;; Reset the font height to the default.
+(kotct/font-reset-height)
 
 ;; Any matching parenthesis is highlighted
 (show-paren-mode +1)

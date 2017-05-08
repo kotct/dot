9f868b91c5930ffb4a5f45290ae68b00a96ce2db
Kristofer Rye <kristofer.rye@gmail.com>
2017-05-08 13:17:01 -0500

    frame-c: Use a function quote for fboundp conditional

--- .emacs.d/lisp/visual/frame-c.el
+++ .emacs.d/lisp/visual/frame-c.el
@@ -1,7 +1,7 @@
 ;; disable tool bar and menu bar
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(if (fboundp 'scroll-bar-mode)
+(if (fboundp #'scroll-bar-mode)
     (scroll-bar-mode -1))
 
 (provide 'frame-c)

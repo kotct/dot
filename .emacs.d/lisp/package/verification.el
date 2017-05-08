ae1bd967422468fc8373fe29f88c25d92f572c6c
Christopher Cooper <christopher.c.cooper@gmail.com>
2017-05-08 16:11:09 -0400

    if we don't have all the packages on headless init, wait for a frame
    we notice this issue in `#'kotct/check-dependency-list` and throw
    `'daemon-mode` back to `init.el`. we then stop what we're doing and
    put everything on the `after-make-frame-functions` hook. when the
    frame is created, we prompt about installing missing packages and
    finish loading.
    
    haven't tested yet with if user configs are missing packages at a
    headless startup. that's next.

--- .emacs.d/lisp/package/verification.el
+++ .emacs.d/lisp/package/verification.el
@@ -1,15 +1,19 @@
 (require 'cl)
 
 ;; check to make sure all dependencies are installed before continuing
-(defun kotct/check-dependency-list ()
-    (if (not (every #'package-installed-p kotct/dependency-list))
-        (progn
-          (y-or-n-p "You don't seem to have all necessary packages installed. Install them?\n(Your emacs will probably not work if you don't.)")
-          ;; since loaddefs don't get evaluated until after init, we must
-          ;; manually require packup (normally loaded via autoloads)
-          (require 'packup)
-          (kotct/packup-install-dependencies nil))))
+(defun kotct/check-dependency-list (&optional frame)
+  ;; (if frame (error "%s" frame))
+  (if (not (every #'package-installed-p kotct/dependency-list))
+      (if (string= (terminal-name (or frame (frame-terminal))) "initial_terminal")
+          (throw 'daemon-mode 'daemon-mode)
+        (y-or-n-p "You don't seem to have all necessary packages installed. Install them?\n(Your emacs will probably not work if you don't.)")
+        ;; since loaddefs don't get evaluated until after init, we must
+        ;; manually require packup (normally loaded via autoloads)
+        (require 'packup)
+        (kotct/packup-install-dependencies nil))))
+
+;; provide before checking, so that if checking fails (i.e. we're in
+;; daemon mode) we know that we have the feature
+(provide 'verification)
 
 (kotct/check-dependency-list)
-
-(provide 'verification)

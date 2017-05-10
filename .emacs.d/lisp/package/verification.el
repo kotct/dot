312be161768b733bdf1756f884c24b39eb416bf8
Christopher Cooper <christopher.c.cooper@gmail.com>
2017-05-10 17:49:18 -0400

    small language update

--- .emacs.d/lisp/package/verification.el
+++ .emacs.d/lisp/package/verification.el
@@ -12,7 +12,7 @@ Typically this happens in init.el."
   (if (not (every #'package-installed-p kotct/dependency-list))
       (if (string= (terminal-name (frame-terminal frame)) "initial_terminal")
           (throw 'daemon-mode 'daemon-mode)
-        (y-or-n-p "You don't seem to have all necessary packages installed. Install them?\n(Your emacs will probably not work if you don't.)")
+        (y-or-n-p "You don't seem to have all necessary packages installed. Install them?\n(This configuration will probably not work if you don't.)")
         ;; since loaddefs don't get evaluated until after init, we must
         ;; manually require packup (normally loaded via autoloads)
         (require 'packup)

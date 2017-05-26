21ac9fef11096a4a3570b89d48f825bf8c75f7dd
Kristofer Rye <kristofer.rye@gmail.com>
2017-05-26 18:16:13 -0500

    Refactor defuns to fix some problems

--- .emacs.d/lisp/user/machine-local.el
+++ .emacs.d/lisp/user/machine-local.el
@@ -1,6 +1,6 @@
 (defun kotct/dot-config-directory ()
   "Returns the directory name for where our machine-local Dot configuration directory should be."
-  (concat (file-name-directory (or (getenv "XDG_CONFIG_HOME") "~/.config")) "dot/"))
+  (concat (or (getenv "XDG_CONFIG_HOME") "~/.config/") "dot/"))
 
 (defun kotct/dot-emacs-config-directory ()
   "Returns the directory name for where our machine-local Emacs subconfiguration directory should be."
@@ -8,7 +8,7 @@
         (emacs-subdir "emacs/"))
     (concat dot-config-dir emacs-subdir)))
 
-(let (emacs-config-directory (kotct/dot-emacs-config-directory))
-  (load (concat emacs-config-directory "init") 'noerror nil nil 'must-suffix))
+(let ((emacs-config-directory (kotct/dot-emacs-config-directory)))
+  (load (concat (file-name-directory emacs-config-directory) "init") 'noerror nil nil 'must-suffix))
 
 (provide 'machine-local)

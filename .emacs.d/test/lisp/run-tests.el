f5fa6631fcab9027d2f0fe3787b4749481d75754
Christopher Cooper <christopher.c.cooper@gmail.com>
2017-07-16 19:31:29 -0400

    add coverage reporting with undercover/coveralls

--- .emacs.d/test/lisp/run-tests.el
+++ .emacs.d/test/lisp/run-tests.el
@@ -2,6 +2,24 @@
 
 (package-initialize)
 
+(unless (package-installed-p 'undercover)
+  (require 'repositories "~/.emacs.d/lisp/package/repositories")
+  (package-refresh-contents)
+  (package-install 'undercover 'dont-select))
+
 (load-file (concat (file-name-directory load-file-name) "/dot-tests.el"))
+
+(require 'undercover)
+;; undercover can't search for matching files recursively
+;; override a local function that fixes this
+(advice-add
+ #'undercover--wildcards-to-files :override
+ (lambda (wildcards)
+   (mapcar (lambda (filename)
+             ;; undercover adds a leading "/", so remove the
+             ;; "/" at the beginning of the absolute filename
+             (substring (file-truename filename) 1))
+           (directory-files-recursively "~/.emacs.d/lisp/" ".el$"))))
+(undercover)
 
 (kotct/run-tests)

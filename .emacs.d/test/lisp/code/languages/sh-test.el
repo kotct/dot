a1cab2dfa64c6638a6baef67a010a105ecdebb95
Kristofer Rye <kristofer.rye@gmail.com>
2017-08-08 19:17:46 -0500

    Add the rest of the specs for the sh configuration
    Note that I did not include a test for `sh-mode-hook` having `indent-tabs-mode` set to `t` by default.  This is primarily due to concerns about equality checking, since it currently runs a `lambda`.  I plan to make API changes to `indentation` to improve all of this, however, so it might be best to ship it like this.

--- .emacs.d/test/lisp/code/languages/sh-test.el
+++ .emacs.d/test/lisp/code/languages/sh-test.el
@@ -4,4 +4,8 @@
   (it "adds a tab variable setter for `sh-basic-offset'"
     (expect (assoc 'sh-basic-offset kotct/tab-variable-setters) :to-be-truthy))
   (it "adds a tab variable setter for `sh-indentation'"
-    (expect (assoc 'sh-indentation kotct/tab-variable-setters) :to-be-truthy)))
+    (expect (assoc 'sh-indentation kotct/tab-variable-setters) :to-be-truthy))
+  (it "sets the indentation for case labels to 0"
+    (expect sh-indent-for-case-label :to-be 0))
+  (it "sets the indentation for case alts to be one level"
+    (expect sh-indent-for-case-alt :to-be '+)))

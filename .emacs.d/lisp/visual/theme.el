4a535ee1391e4f4eff3e4e3d23c6aa84fa460ca7
Kristofer Rye <kristofer.rye@gmail.com>
2017-10-03 12:59:18 -0500

    Add `force-load` optional argument to `kotct/switch-to-theme`
    This will be used to instruct `kotct/switch-to-theme` to load the file
    without worrying about whether or not it is already known.
    
    For performance reasons, this is optional, (and will be nil in all
    instances where it is already used) because loading a theme over and
    over again requires evaluation.

--- .emacs.d/lisp/visual/theme.el
+++ .emacs.d/lisp/visual/theme.el
@@ -2,8 +2,10 @@
   nil
   "The currently enabled theme.")
 
-(defun kotct/switch-to-theme (theme)
-  "Enable the theme THEME. Disable the current theme."
+(defun kotct/switch-to-theme (theme &optional force-load)
+  "Enable the theme THEME. Disable the current theme.
+
+If FORCE-LOAD is set, THEME is always loaded."
   (if kotct/current-theme (disable-theme kotct/current-theme))
   (if (member theme custom-known-themes)
       (enable-theme theme)

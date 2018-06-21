4e807472d7b78843b7daab5dbd67ab0883080101
samontea <merciers.merciers@gmail.com>
2018-06-21 16:40:57 -0400

    change to avy

--- .emacs.d/lisp/package/dependencies.el
+++ .emacs.d/lisp/package/dependencies.el
@@ -11,8 +11,8 @@
     auto-complete ;; code autocompletion
     markdown-mode ;; major mode for markdown
     smart-tabs-mode ;; indentation with tabs and spaces
-    ace-jump-mode ;; immediately jump to any visible character
-    ace-jump-zap ;; zap-to-char but harnessing ace-jump
+    avy ;; immediately jump to any visible character
+    avy-zap ;; zap-to-char but harnessing ace-jump
     editorconfig ;; editorconfig plugin
     hl-todo ;; highlight TODO, FIXME, FAIL, etc.
     highlight-symbol ;; navigate between and highlight symbols

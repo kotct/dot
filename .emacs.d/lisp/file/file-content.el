bbfa10c6a36920f28e9a8f8d48c2749b1a537a12
Kristofer Rye <kristofer.rye@gmail.com>
2018-07-23 08:54:26 -0500

    Prefer the UTF-8 coding system
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- .emacs.d/lisp/file/file-content.el
+++ .emacs.d/lisp/file/file-content.el
@@ -3,4 +3,7 @@
 ;; M-x set-variable RET require-final-newline RET nil RET
 (setq-default require-final-newline t)
 
+;; UTF-8 is a sane default
+(prefer-coding-system 'utf-8)
+
 (provide 'file-content)

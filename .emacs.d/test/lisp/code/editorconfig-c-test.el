7526618ec61f59cf2abb5154f87d9f4a2565df45
Kristofer Rye <kristofer.rye@gmail.com>
2020-07-24 14:29:54 -0500

    test: Add structure of tests for kotct/check-editorconfig-props
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- .emacs.d/test/lisp/code/editorconfig-c-test.el
+++ .emacs.d/test/lisp/code/editorconfig-c-test.el
@@ -1,1 +1,8 @@
 (kotct/load-corresponding)
+
+(describe "kotct/check-editorconfig-props"
+  (describe "on a file with no corresponding editorconfig props"
+    (describe "when kotct/warn-on-editorconfig-with-no-props is truthy"
+      (it "prints a warning"))
+    (describe "when kotct/warn-on-editorconfig-with-no-props is not truthy"
+      (it "doesn't print a warning"))))

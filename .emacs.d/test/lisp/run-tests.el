c369ba02579a2184f8ade6637c49f2db55425f65
Kristofer Rye <kristofer.rye@gmail.com>
2020-07-24 20:02:33 -0500

    ci: Make coverage report file a bit more straightforward
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- .emacs.d/test/lisp/run-tests.el
+++ .emacs.d/test/lisp/run-tests.el
@@ -4,22 +4,25 @@
 
 (load-file (concat (file-name-directory load-file-name) "/dot-tests.el"))
 
-(when (require 'undercover nil 'noerror)
-  ;; undercover can't search for matching files recursively
-  ;; override a local function that fixes this
-  (advice-add
-   #'undercover--wildcards-to-files :override
-   (lambda (_wildcards)
-     (mapcar (lambda (filename)
-               ;; undercover adds a leading "/", so remove the
-               ;; "/" at the beginning of the absolute filename
-               (substring (file-truename filename) 1))
-             (directory-files-recursively "~/.emacs.d/lisp/" ".el$"))))
-  (undercover (:report-file (concat
-			     (file-name-directory
-			      (file-name-directory
-			       (file-name-directory
-				(file-name-directory load-file-name)))) "/coverage-final.json"))
-	      (:send-report nil)))
+(let ((coverage-report-file
+       (concat
+        (file-name-directory
+         (file-name-directory
+          (file-name-directory
+           (file-name-directory load-file-name)))) "/coverage-final.json")))
+  (when (require 'undercover nil 'noerror)
+    ;; undercover can't search for matching files recursively
+    ;; override a local function that fixes this
+    (advice-add
+     #'undercover--wildcards-to-files :override
+     (lambda (_wildcards)
+       (mapcar (lambda (filename)
+                 ;; undercover adds a leading "/", so remove the
+                 ;; "/" at the beginning of the absolute filename
+                 (substring (file-truename filename) 1))
+               (directory-files-recursively "~/.emacs.d/lisp/" ".el$"))))
+    (message (format "Running undercover, writing report to %s" coverage-report-file))
+    (undercover (:report-file coverage-report-file)
+                (:send-report nil))))
 
 (kotct/run-tests)

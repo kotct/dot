a251d6202979e2ed7df07c322bfd1843a1674fa5
Kristofer Rye <kristofer.rye@gmail.com>
2020-08-01 12:10:11 -0500

    ci: Overhaul undercover file location
    Previously, the ~/.emacs.d/lisp/ directory was consulted, but this
    required the code to be installed into that directory and the script
    file to be executed via that path.  (This meant that executing the
    script directly instead of via the installed symlink to ~/.emacs.d/
    would not work.)
    
    Now, we simply expand all files and pass that as an argument list to
    the undercover entrypoint.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- .emacs.d/test/lisp/run-tests.el
+++ .emacs.d/test/lisp/run-tests.el
@@ -7,23 +7,20 @@
 
 (load-file (concat (expanded-parent-dir load-file-name) "/dot-tests.el"))
 
-(let ((coverage-report-file
-       (concat
-        (parent-dir (parent-dir (parent-dir (parent-dir load-file-name)))) "coverage-final.json"))
-      (undercover-force-coverage t))
-  (when (require 'undercover nil 'noerror)
-    ;; undercover can't search for matching files recursively
-    ;; override a local function that fixes this
-    (advice-add
-     #'undercover--wildcards-to-files :override
-     (lambda (_wildcards)
-       (mapcar (lambda (filename)
-                 ;; undercover adds a leading "/", so remove the
-                 ;; "/" at the beginning of the absolute filename
-                 (substring (file-truename filename) 1))
-               (directory-files-recursively "~/.emacs.d/lisp/" ".el$"))))
-    (message (format "Running undercover, writing report to %s" coverage-report-file))
-    (undercover (:report-file coverage-report-file)
-                (:send-report nil))))
+(defun kotct/locate-dot-el-files
+  (directory-files-recursively
+    (concat
+      (expanded-parent-dir ;; .emacs.d/
+        (expanded-parent-dir ;; .emacs.d/test/
+          (expanded-parent-dir ;; .emacs.d/test/lisp/
+            load-file-name))) ;; .emacs.d/test/lisp/run-tests.el
+      "lisp/") ;; .emacs.d/lisp/
+    ".el$"))
+
+(when (require 'undercover nil t)
+  (undercover
+    (kotct/locate-dot-el-files)
+    (:report-file "coverage-final.json")
+    (:send-report nil)))
 
 (kotct/run-tests)

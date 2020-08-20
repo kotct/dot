kotct/dot

commit 7324870b6c12a17d08438ba1ad8368e41bc6647b

fix improper let scope

.emacs.d/lisp/package/git-update.el
@@ -49,17 +49,19 @@ Returns true if an update occured."
               (insert (format "%s" (kotct/git-repository-diff directory)))
               (pop-to-buffer-same-window buffer)
               ;; prompt user about updating and update if they want
-              (let updating (or auto-update
-                                (y-or-n-p (format "Pull the most recent changes to %s?" directory))))
-              (if updating
-                  (let ((default-directory directory))
-                    (kotct/run-git "pull" "--rebase" "origin" "master")
-                    (message (format "%s updated!" directory)))
-                (message "Okay we promise we didn't do anything!"))
-              ;; reset buffer what not
-              (kill-buffer buffer)
-              (set-buffer old-buffer)
-              updating))
+              (let ((updating (or auto-update
+                                  (y-or-n-p (format "Pull the most recent changes to %s?" directory)))))
+
+                (if updating
+                    (let ((default-directory directory))
+                      (kotct/run-git "pull" "--rebase" "origin" "master")
+                      (message (format "%s updated!" directory)))
+                  (message "Okay we promise we didn't do anything!"))
+
+                ;; reset buffer what not
+                (kill-buffer buffer)
+                (set-buffer old-buffer)
+                updating)))
         (message (format "%s is up to date." directory))
         nil)
     (error (format "Error: kotct/update-git-repository doesn't support updating non-master branches, and %s is on branch %s."

commit 2fb2f3e351bf63bafe066f42946032b9d51ff34e

fix: dependency issues introduced in #125

.emacs.d/lisp/package/git-update.el
@@ -3,9 +3,20 @@
 ;;;  kotct/update-dot-config
 ;;;  kotct/update-user-config
 
-(require 'user-config-system)
-
 (package-initialize)
+
+(defun kotct/run-git (&rest args)
+  "Run a git command, specified by ARGS."
+  (let ((process-environment (cons "GIT_TERMINAL_PROMPT=0" process-environment)))
+    (with-temp-buffer
+      (let ((exit-code (apply 'call-process
+                              (append (list (executable-find "git")
+                                            nil (current-buffer) nil
+                                            "--no-pager" )
+                                      args))))
+        (if (zerop exit-code)
+            (buffer-string)
+          (error "Error running git %s\n%s" args (buffer-string)))))))
 
 (defvar kotct/directory "~/.emacs.d"
   "The path to your instance of kotct/dot or one of it's subdirectories.")

.emacs.d/lisp/user/user-config-system.el
@@ -1,3 +1,5 @@
+(require 'git-update)
+
 ;;; C-x C-z: switch personal configs
 
 (defvar kotct/user-current-username
@@ -12,19 +14,6 @@
   "Appends PACKAGES to `kotct/dependency-list'"
   (setf kotct/dependency-list (append kotct/dependency-list packages))
   (kotct/check-dependency-list))
-
-(defun kotct/run-git (&rest args)
-  "Run a git command, specified by ARGS."
-  (let ((process-environment (cons "GIT_TERMINAL_PROMPT=0" process-environment)))
-    (with-temp-buffer
-      (let ((exit-code (apply 'call-process
-                              (append (list (executable-find "git")
-                                            nil (current-buffer) nil
-                                            "--no-pager" )
-                                      args))))
-        (if (zerop exit-code)
-            (buffer-string)
-          (error "Error running git %s\n%s" args (buffer-string)))))))
 
 (defun kotct/user-get-default-username ()
   "Look up the default username set on this machine."
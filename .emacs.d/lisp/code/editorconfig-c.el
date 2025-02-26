0c24ea4eba41172a341baf0f9444671a25a3f39d
Christopher Cooper <christopher@cg505.com>
2025-02-26 10:53:26 -0800

    use boundp instead of fboundp
    follow up to #153

--- .emacs.d/lisp/code/editorconfig-c.el
+++ .emacs.d/lisp/code/editorconfig-c.el
@@ -21,7 +21,7 @@ and is ignored."
 
 Debug warning is suppressed if SUPPRESS is non-nil."
 
-  (if (and (not suppress) (fboundp 'editorconfig-exec-path))
+  (if (and (not suppress) (boundp 'editorconfig-exec-path))
       (if (not (executable-find editorconfig-exec-path))
           (message "EditorConfig C Core not found in `editorconfig-exec-path'."))))
 
